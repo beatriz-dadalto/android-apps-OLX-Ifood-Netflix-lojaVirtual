fa3b32e69966bf65bdc22f06a51f34df397dd127
Beatriz Dadalto <biacoelho@proton.me>
2022-08-30 19:02:40 -0300

    Criando Fragment de Opcoes de Empresa

---
+++ IfoodCloneApp/app/src/main/java/com/biamailov3/ifoodclone/activity/empresa/EmpresaEntregasActivity.java
@@ -0,0 +1,16 @@
+package com.biamailov3.ifoodclone.activity.empresa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.biamailov3.ifoodclone.R;
+
+public class EmpresaEntregasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empresa_entregas);
+    }
+}
--- IfoodCloneApp/app/src/main/java/com/biamailov3/ifoodclone/fragment/empresa/EmpresaConfigFragment.java
+++ IfoodCloneApp/app/src/main/java/com/biamailov3/ifoodclone/fragment/empresa/EmpresaConfigFragment.java
@@ -1,5 +1,6 @@
 package com.biamailov3.ifoodclone.fragment.empresa;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +8,76 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.biamailov3.ifoodclone.R;
+import com.biamailov3.ifoodclone.activity.empresa.EmpresaAddMaisActivity;
+import com.biamailov3.ifoodclone.activity.empresa.EmpresaCategoriasActivity;
+import com.biamailov3.ifoodclone.activity.empresa.EmpresaConfigActivity;
+import com.biamailov3.ifoodclone.activity.empresa.EmpresaEnderecoActivity;
+import com.biamailov3.ifoodclone.activity.empresa.EmpresaEntregasActivity;
+import com.biamailov3.ifoodclone.activity.empresa.EmpresaRecebimentosActivity;
+import com.biamailov3.ifoodclone.activity.usuario.UsuarioHomeActivity;
+import com.biamailov3.ifoodclone.helper.FirebaseHelper;
+import com.squareup.picasso.Picasso;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EmpresaConfigFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class EmpresaConfigFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public EmpresaConfigFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EmpresaConfigFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EmpresaConfigFragment newInstance(String param1, String param2) {
-        EmpresaConfigFragment fragment = new EmpresaConfigFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private ImageView imgLogo;
+    private TextView textEmpresa;
+    private LinearLayout menuEmpresa;
+    private LinearLayout menuCategorias;
+    private LinearLayout menuRecebimentos;
+    private LinearLayout menuAddMais;
+    private LinearLayout menuEndereco;
+    private LinearLayout menuEntregas;
+    private LinearLayout menuDeslogar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_empresa_config, container, false);
+        View view = inflater.inflate(R.layout.fragment_empresa_config, container, false);
+        
+        iniciarComponentes(view);
+        configCliques();
+        configAcesso();
+
+        return view;
+    }
+
+    private void configAcesso() {
+        Picasso.get().load(FirebaseHelper.getAuth().getCurrentUser().getPhotoUrl()).into(imgLogo);
+        textEmpresa.setText(FirebaseHelper.getAuth().getCurrentUser().getDisplayName());
+    }
+
+    private void configCliques() {
+        menuEmpresa.setOnClickListener(view -> startActivity(new Intent(requireContext(), EmpresaConfigActivity.class)));
+        menuCategorias.setOnClickListener(view -> startActivity(new Intent(requireContext(), EmpresaCategoriasActivity.class)));
+        menuRecebimentos.setOnClickListener(view -> startActivity(new Intent(requireContext(), EmpresaRecebimentosActivity.class)));
+        menuAddMais.setOnClickListener(view -> startActivity(new Intent(requireContext(), EmpresaAddMaisActivity.class)));
+        menuEndereco.setOnClickListener(view -> startActivity(new Intent(requireContext(), EmpresaEnderecoActivity.class)));
+        menuEntregas.setOnClickListener(view -> startActivity(new Intent(requireContext(), EmpresaEntregasActivity.class)));
+        menuDeslogar.setOnClickListener(view -> deslogar());
+    }
+
+    private void deslogar() {
+        FirebaseHelper.getAuth().signOut();
+        requireActivity().finish();
+        startActivity(new Intent(requireActivity(), UsuarioHomeActivity.class));
+    }
+
+    private void iniciarComponentes(View view) {
+        imgLogo = view.findViewById(R.id.img_logo);
+        textEmpresa = view.findViewById(R.id.text_empresa);
+        menuEmpresa = view.findViewById(R.id.menu_empresa);
+        menuCategorias = view.findViewById(R.id.menu_categorias);
+        menuRecebimentos = view.findViewById(R.id.menu_recebimentos);
+        menuAddMais = view.findViewById(R.id.menu_add_mais);
+        menuEndereco = view.findViewById(R.id.menu_endereco);
+        menuEntregas = view.findViewById(R.id.menu_entregas);
+        menuDeslogar = view.findViewById(R.id.menu_deslogar);
     }
 }
