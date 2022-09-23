9dc0b4f1b0dbea2110dcfd27acb31c66f89d3fe0
Beatriz Dadalto <biacoelho@proton.me>
2022-09-23 10:53:27 -0300

    Criando layout fragment financeiro da empresa

--- IfoodCloneApp/app/src/main/java/com/biamailov3/ifoodclone/fragment/empresa/EmpresaFinanceiroFragment.java
+++ IfoodCloneApp/app/src/main/java/com/biamailov3/ifoodclone/fragment/empresa/EmpresaFinanceiroFragment.java
@@ -3,64 +3,48 @@ package com.biamailov3.ifoodclone.fragment.empresa;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.biamailov3.ifoodclone.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EmpresaFinanceiroFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class EmpresaFinanceiroFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public EmpresaFinanceiroFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EmpresaFinanceiroFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EmpresaFinanceiroFragment newInstance(String param1, String param2) {
-        EmpresaFinanceiroFragment fragment = new EmpresaFinanceiroFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private EditText edtInicio;
+    private EditText edtFinal;
+    private Button btnFiltrar;
+    private Button btnTodos;
+    private ProgressBar progressBar;
+    private TextView textReceita;
+    private TextView textSaldo;
+    private RecyclerView rvDespesas;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_empresa_financeiro, container, false);
+        View view = inflater.inflate(R.layout.fragment_empresa_financeiro, container, false);
+
+        iniciarComponentes(view);
+
+        return view;
+    }
+
+    private void iniciarComponentes(View view) {
+        edtInicio = view.findViewById(R.id.edt_inicio);
+        edtFinal = view.findViewById(R.id.edt_final);
+        btnFiltrar = view.findViewById(R.id.btn_filtrar);
+        btnTodos = view.findViewById(R.id.btn_todos);
+        progressBar = view.findViewById(R.id.progressBar);
+        textReceita = view.findViewById(R.id.text_receita);
+        textSaldo = view.findViewById(R.id.text_saldo);
+        rvDespesas = view.findViewById(R.id.rv_despesas);
     }
 }
