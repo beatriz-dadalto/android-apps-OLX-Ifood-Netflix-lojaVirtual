0af57d702f09dbe434620f1557ea1fe9c71c26a8
Beatriz Dadalto <biacoelho@proton.me>
2022-09-25 19:50:48 -0300

    Criando layout de CadastroActivity

---
+++ BancoDigitalApp/app/src/main/java/com/br/bancodigital/autenticacao/CadastroActivity.java
@@ -0,0 +1,18 @@
+package com.br.bancodigital.autenticacao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.br.bancodigital.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+
+
+}
