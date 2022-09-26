ad511c26c745cbbd8ef040af734cc7954e8335e6
Beatriz Dadalto <biacoelho@proton.me>
2022-09-26 00:18:20 -0300

    Criando SplashActivity

---
+++ BancoDigitalApp/app/src/main/java/com/br/bancodigital/app/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.br.bancodigital.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.br.bancodigital.MainActivity;
+import com.br.bancodigital.R;
+import com.br.bancodigital.autenticacao.LoginActivity;
+import com.br.bancodigital.helper.FirebaseHelper;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(this::getAutenticacao, 3000);
+    }
+
+    private void getAutenticacao() {
+        if (FirebaseHelper.getAutenticado()) {
+            startActivity(new Intent(this, MainActivity.class));
+        } else {
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+        finish();
+    }
+}
