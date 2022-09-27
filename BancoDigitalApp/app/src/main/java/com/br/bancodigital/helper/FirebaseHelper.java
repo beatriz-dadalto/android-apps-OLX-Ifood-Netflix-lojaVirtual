9cf8446953e3f55bfe2f44970f681e6afc5deef1
Beatriz Dadalto <biacoelho@proton.me>
2022-09-27 11:35:32 -0300

    Salvando e exibindo imagem de perfil

--- BancoDigitalApp/app/src/main/java/com/br/bancodigital/helper/FirebaseHelper.java
+++ BancoDigitalApp/app/src/main/java/com/br/bancodigital/helper/FirebaseHelper.java
@@ -3,11 +3,21 @@ package com.br.bancodigital.helper;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class FirebaseHelper {
 
     private static FirebaseAuth auth;
     private static DatabaseReference databaseReference;
+    private static StorageReference storageReference;
+
+    public static StorageReference getStorageReference(){
+        if(storageReference == null){
+            storageReference = FirebaseStorage.getInstance().getReference();
+        }
+        return storageReference;
+    }
 
     public static String getIdFirebase(){
         return getAuth().getUid();
--- BancoDigitalApp/app/src/main/java/com/br/bancodigital/usuario/MinhaContaActivity.java
+++ BancoDigitalApp/app/src/main/java/com/br/bancodigital/usuario/MinhaContaActivity.java
@@ -1,12 +1,20 @@
 package com.br.bancodigital.usuario;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,13 +24,26 @@ import com.br.bancodigital.app.MainActivity;
 import com.br.bancodigital.helper.FirebaseHelper;
 import com.br.bancodigital.model.Usuario;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.normal.TedPermission;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MinhaContaActivity extends AppCompatActivity {
+
+    private final int REQUEST_GALERIA = 100;
 
     private EditText edtNome;
     private EditText edtEmail;
     private EditText edtTelefone;
     private ProgressBar progressBar;
+    private ImageView imagemPerfil;
+
+    private String caminhoImagem;
 
     private Usuario usuario;
 
@@ -34,6 +55,73 @@ public class MinhaContaActivity extends AppCompatActivity {
         configToolbar();
         iniciaComponentes();
         configDados();
+        configCliques();
+    }
+
+    private void configCliques() {
+        imagemPerfil.setOnClickListener(v -> verificaPermissaoGaleria());
+    }
+
+    private void verificaPermissaoGaleria() {
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                abrirGaleria();
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(MinhaContaActivity.this, "Permissão negada!", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        // ALERT DIALOG
+        TedPermission.create()
+                .setPermissionListener(permissionListener)
+                .setDeniedTitle("Permissão negada.")
+                .setDeniedMessage("Se você não aceitar a permissão não poderá acessar a galeria do dispositivo, deseja ativar o acesso agora?")
+                .setDeniedCloseButtonText("Não")
+                .setGotoSettingButtonText("Sim")
+                .setPermissions(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .check();
+    }
+
+    private void abrirGaleria() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, REQUEST_GALERIA);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_GALERIA) {
+
+                Bitmap bitmap;
+
+                Uri imagemSelecionada = data.getData();
+                caminhoImagem = data.getData().toString();
+
+                if (Build.VERSION.SDK_INT < 28) {
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imagemSelecionada);
+                        imagemPerfil.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(), imagemSelecionada);
+
+                    try {
+                        bitmap = ImageDecoder.decodeBitmap(source);
+                        imagemPerfil.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 
     public void validaDados(View view) {
@@ -49,7 +137,11 @@ public class MinhaContaActivity extends AppCompatActivity {
                 usuario.setNome(nome);
                 usuario.setTelefone(telefone);
 
-                salvarDadosUsuario();
+                if (caminhoImagem != null) {
+                    salvarImagemFirebase();
+                } else {
+                    salvarDadosUsuario();
+                }
 
             } else {
                 edtTelefone.requestFocus();
@@ -59,6 +151,22 @@ public class MinhaContaActivity extends AppCompatActivity {
             edtNome.requestFocus();
             edtNome.setError("Informe o nome");
         }
+    }
+
+    private void salvarImagemFirebase() {
+        StorageReference storageReference = FirebaseHelper.getStorageReference()
+                .child("imagens")
+                .child("perfil")
+                .child(FirebaseHelper.getIdFirebase() + ".jpeg");
+
+        UploadTask uploadTask = storageReference.putFile(Uri.parse(caminhoImagem));
+        uploadTask.addOnSuccessListener(taskSnapshot -> storageReference.getDownloadUrl()
+                .addOnCompleteListener(task -> {
+
+                    usuario.setUrlImagem(task.getResult().toString());
+                    salvarDadosUsuario();
+
+                })).addOnFailureListener(e -> Toast.makeText(this, FirebaseHelper.validaErros(e.getMessage()), Toast.LENGTH_SHORT).show());
     }
 
     private void salvarDadosUsuario() {
@@ -81,6 +189,12 @@ public class MinhaContaActivity extends AppCompatActivity {
         edtTelefone.setText(usuario.getTelefone());
         edtEmail.setText(usuario.getEmail());
 
+        if (usuario.getUrlImagem() != null) {
+            Picasso.get().load(usuario.getUrlImagem())
+                    .placeholder(R.drawable.loading)
+                    .into(imagemPerfil);
+        }
+
         progressBar.setVisibility(View.GONE);
     }
 
@@ -89,6 +203,7 @@ public class MinhaContaActivity extends AppCompatActivity {
         edtEmail = findViewById(R.id.edtEmail);
         edtTelefone = findViewById(R.id.edtTelefone);
         progressBar = findViewById(R.id.progressBar);
+        imagemPerfil = findViewById(R.id.imagemPerfil);
     }
 
     private void configToolbar() {
