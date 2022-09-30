51f29d9c37f9feb6f6dd51846fd619e46e5c35d2
Beatriz Dadalto <biacoelho@proton.me>
2022-09-30 16:25:47 -0300

    Deletando notificacoes com Swipe

--- BancoDigitalApp/app/src/main/java/com/br/bancodigital/notificacoes/NotificacoesActivity.java
+++ BancoDigitalApp/app/src/main/java/com/br/bancodigital/notificacoes/NotificacoesActivity.java
@@ -1,6 +1,7 @@
 package com.br.bancodigital.notificacoes;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,8 +9,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.br.bancodigital.R;
 import com.br.bancodigital.adapter.NotificacaoAdapter;
@@ -21,6 +24,8 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
+import com.tsuryo.swipeablerv.SwipeLeftRightCallback;
+import com.tsuryo.swipeablerv.SwipeableRecyclerView;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -31,9 +36,11 @@ public class NotificacoesActivity extends AppCompatActivity implements Notificac
     private List<Notificacao> notificacaoList = new ArrayList<>();
     private NotificacaoAdapter notificacaoAdapter;
 
-    private RecyclerView rvNotificacoes;
+    private SwipeableRecyclerView rvNotificacoes;
     private ProgressBar progressBar;
     private TextView textInfo;
+
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +58,58 @@ public class NotificacoesActivity extends AppCompatActivity implements Notificac
         rvNotificacoes.setHasFixedSize(true);
         notificacaoAdapter = new NotificacaoAdapter(notificacaoList, getBaseContext(), this);
         rvNotificacoes.setAdapter(notificacaoAdapter);
+
+        rvNotificacoes.setListener(new SwipeLeftRightCallback.Listener() {
+            @Override
+            public void onSwipedLeft(int position) {
+                showDialogRemover(notificacaoList.get(position));
+            }
+
+            @Override
+            public void onSwipedRight(int position) {
+                // TODO notificacao nao lida
+            }
+        });
+    }
+
+    private void showDialogRemover(Notificacao notificacao) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(
+                this, R.style.CustomAlertDialog
+        );
+
+        View view = getLayoutInflater().inflate(R.layout.layout_dialog_delete, null);
+
+        view.findViewById(R.id.btnOK).setOnClickListener(view1 -> {
+            removerNotificacoes(notificacao);
+            dialog.dismiss();
+        });
+
+        view.findViewById(R.id.btnFechar).setOnClickListener(v -> {
+            dialog.dismiss();
+            notificacaoAdapter.notifyDataSetChanged();
+        });
+
+        builder.setView(view);
+
+        dialog = builder.create();
+        dialog.show();
+    }
+
+    private void removerNotificacoes(Notificacao notificacao) {
+        DatabaseReference notificacoesRef = FirebaseHelper.getDatabaseReference()
+                .child("notificacoes")
+                .child(FirebaseHelper.getIdFirebase())
+                .child(notificacao.getId());
+        notificacoesRef.removeValue().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                notificacaoList.remove(notificacao);
+                Toast.makeText(this, "Notificação foi removida!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Erro ao remover a notificação!", Toast.LENGTH_SHORT).show();
+            }
+            notificacaoAdapter.notifyDataSetChanged();
+        });
+
     }
 
     private void recuperaNotificacoes() {
