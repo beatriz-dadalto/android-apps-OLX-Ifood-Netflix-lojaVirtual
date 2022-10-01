a4d115b9c1e1a622a63d28a5b209f1fdbf389803
Beatriz Dadalto <biacoelho@proton.me>
2022-09-30 22:51:31 -0300

    Configuracao Bottom Navigation View

--- NetflixCloneApp/app/src/main/java/com/br/netflix/activity/MainActivity.java
+++ NetflixCloneApp/app/src/main/java/com/br/netflix/activity/MainActivity.java
@@ -1,10 +1,14 @@
 package com.br.netflix.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.br.netflix.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+
+        NavController navController = navHostFragment.getNavController();
+
+        NavigationUI.setupWithNavController(bottomNavigationView ,navController);
+
     }
 }
--- NetflixCloneApp/app/src/main/java/com/br/netflix/activity/SplashActivity.java
+++ NetflixCloneApp/app/src/main/java/com/br/netflix/activity/SplashActivity.java
@@ -2,7 +2,10 @@ package com.br.netflix.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 
 import com.br.netflix.R;
@@ -13,6 +16,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler(getMainLooper()).postDelayed(() -> {
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        }, 2000);
     }
 
     @Override
