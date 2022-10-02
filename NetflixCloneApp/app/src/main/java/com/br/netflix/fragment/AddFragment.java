16cd5afe1a15c451beeb90bafac3039b54f74128
Beatriz Dadalto <biacoelho@proton.me>
2022-10-02 01:52:41 -0300

    Criacao de layout do fragment add

--- NetflixCloneApp/app/src/main/java/com/br/netflix/fragment/AddFragment.java
+++ NetflixCloneApp/app/src/main/java/com/br/netflix/fragment/AddFragment.java
@@ -2,6 +2,8 @@ package com.br.netflix.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -17,5 +19,11 @@ public class AddFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
     }
 }
