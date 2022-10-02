123266e5161e30524371dc29986bf83f3c7a4d13
Beatriz Dadalto <biacoelho@proton.me>
2022-10-02 02:29:11 -0300

    Configuracao lib TedPermission

--- NetflixCloneApp/app/src/main/java/com/br/netflix/fragment/AddFragment.java
+++ NetflixCloneApp/app/src/main/java/com/br/netflix/fragment/AddFragment.java
@@ -1,18 +1,31 @@
 package com.br.netflix.fragment;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.br.netflix.R;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.normal.TedPermission;
+
+import java.util.List;
 
 public class AddFragment extends Fragment {
+
+    private final int SELECAO_GALERIA = 100;
+
+    private ImageView imageView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,5 +38,45 @@ public class AddFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        iniciaComponentes(view);
+        configCliques();
+
+    }
+
+    private void iniciaComponentes(View view) {
+        imageView = view.findViewById(R.id.imageView);
+    }
+
+    private void configCliques() {
+        imageView.setOnClickListener(v -> verificaPermissaoGaleria());
+    }
+
+    private void verificaPermissaoGaleria() {
+        PermissionListener permissionlistener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                abrirGaleria();
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(getActivity(), "Permissão negada.", Toast.LENGTH_LONG).show();
+            }
+        };
+
+        // ALERT DIALOG
+        TedPermission.create()
+                .setPermissionListener(permissionlistener)
+                .setDeniedTitle("Permissões")
+                .setDeniedMessage("Se você não aceitar a permissão não poderá acessar a galeria do celular. Deseja permitir o acesso?")
+                .setDeniedCloseButtonText("Não")
+                .setGotoSettingButtonText("Sim")
+                .setPermissions(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .check();
+    }
+
+    private void abrirGaleria() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, SELECAO_GALERIA);
     }
 }
