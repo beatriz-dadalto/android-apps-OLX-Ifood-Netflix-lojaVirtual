a9ba6c3cfebdbaeeb630461d69f52e6c9449ce7b
Beatriz Dadalto <biacoelho@proton.me>
2022-10-01 22:25:05 -0300

    Criando layout da activity de login

---
+++ NetflixCloneApp/app/src/main/java/com/br/netflix/autenticacao/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.br.netflix.autenticacao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.br.netflix.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- NetflixCloneApp/app/src/main/java/com/br/netflix/fragment/InicioFragment.java
+++ NetflixCloneApp/app/src/main/java/com/br/netflix/fragment/InicioFragment.java
@@ -1,7 +1,10 @@
 package com.br.netflix.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.br.netflix.R;
+import com.br.netflix.autenticacao.LoginActivity;
 
 public class InicioFragment extends Fragment {
 
@@ -17,5 +21,13 @@ public class InicioFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_inicio, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.btnLogin).setOnClickListener(view1 ->
+                startActivity(new Intent(requireActivity(), LoginActivity.class)));
     }
 }
