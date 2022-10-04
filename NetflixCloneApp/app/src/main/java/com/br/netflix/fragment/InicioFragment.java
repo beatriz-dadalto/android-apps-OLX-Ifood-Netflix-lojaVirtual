1d192c4dfd8bfed648df17c37a6a5755d2e77acc
Beatriz Dadalto <biacoelho@proton.me>
2022-10-03 22:13:34 -0300

    Criacao de layout da fragment home

--- NetflixCloneApp/app/src/main/java/com/br/netflix/fragment/InicioFragment.java
+++ NetflixCloneApp/app/src/main/java/com/br/netflix/fragment/InicioFragment.java
@@ -6,17 +6,34 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.br.netflix.R;
+import com.br.netflix.adapter.AdapterCategoria;
 import com.br.netflix.autenticacao.LoginActivity;
 import com.br.netflix.helper.FirebaseHelper;
+import com.br.netflix.model.Categoria;
+import com.br.netflix.model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InicioFragment extends Fragment {
+
+    private AdapterCategoria adapterCategoria;
+
+    private List<Categoria> categoriaList = new ArrayList<>();
+    private List<Post> postList = new ArrayList<>();
+
+    private RecyclerView rvCategorias;
+    private ProgressBar progressBar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,5 +53,20 @@ public class InicioFragment extends Fragment {
                 startActivity(new Intent(requireActivity(), LoginActivity.class));
             }
         });
+
+        iniciaComponentes(view);
+        configRv();
+    }
+
+    private void configRv() {
+        rvCategorias.setLayoutManager(new LinearLayoutManager(getContext()));
+        rvCategorias.setHasFixedSize(true);
+        adapterCategoria = new AdapterCategoria(categoriaList, getContext());
+        rvCategorias.setAdapter(adapterCategoria);
+    }
+
+    private void iniciaComponentes(View view) {
+        rvCategorias = view.findViewById(R.id.rvCategorias);
+        progressBar = view.findViewById(R.id.progressBar);
     }
 }
