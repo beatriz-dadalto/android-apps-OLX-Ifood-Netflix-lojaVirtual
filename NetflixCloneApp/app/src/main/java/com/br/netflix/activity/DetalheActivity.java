abdd548493ca833cef6233eeab2a1aa90351e5f4
Beatriz Dadalto <biacoelho@proton.me>
2022-10-05 00:10:04 -0300

    Exibindo posts semelhantes na tela de detalhes

--- NetflixCloneApp/app/src/main/java/com/br/netflix/activity/DetalheActivity.java
+++ NetflixCloneApp/app/src/main/java/com/br/netflix/activity/DetalheActivity.java
@@ -1,7 +1,10 @@
 package com.br.netflix.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -10,12 +13,25 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.br.netflix.R;
+import com.br.netflix.adapter.AdapterCategoria;
+import com.br.netflix.adapter.AdapterPost;
+import com.br.netflix.helper.FirebaseHelper;
+import com.br.netflix.model.Categoria;
 import com.br.netflix.model.Post;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DetalheActivity extends AppCompatActivity {
+
+    private List<Post> postList = new ArrayList<>();
+    private AdapterPost adapterPost;
 
     private TextView textTitulo;
     private ImageView imagemPost;
@@ -36,12 +52,41 @@ public class DetalheActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detalhe);
 
         iniciaComponentes();
+        configRv();
         configDados();
         configCliques();
+        recuperaPosts();
     }
 
     private void configCliques() {
         findViewById(R.id.ibVoltar).setOnClickListener(view -> finish());
+    }
+
+    private void recuperaPosts() {
+        DatabaseReference postsRef = FirebaseHelper.getDatabaseReference()
+                .child("posts");
+        postsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    postList.add(ds.getValue(Post.class));
+                }
+
+                adapterPost.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void configRv() {
+        rvPosts.setLayoutManager(new GridLayoutManager(this, 3));
+        rvPosts.setHasFixedSize(true);
+        adapterPost = new AdapterPost(postList, this);
+        rvPosts.setAdapter(adapterPost);
     }
 
     private void configDados() {
