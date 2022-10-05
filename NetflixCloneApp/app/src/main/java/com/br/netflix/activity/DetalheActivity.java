93dda13046e61cf96c4117f4a7132449e67ccb1d
Beatriz Dadalto <biacoelho@proton.me>
2022-10-05 17:06:28 -0300

    Realizando downloads dos posts

--- NetflixCloneApp/app/src/main/java/com/br/netflix/activity/DetalheActivity.java
+++ NetflixCloneApp/app/src/main/java/com/br/netflix/activity/DetalheActivity.java
@@ -11,12 +11,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.br.netflix.R;
 import com.br.netflix.adapter.AdapterCategoria;
 import com.br.netflix.adapter.AdapterPost;
 import com.br.netflix.helper.FirebaseHelper;
 import com.br.netflix.model.Categoria;
+import com.br.netflix.model.Download;
 import com.br.netflix.model.Post;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -30,6 +32,7 @@ import java.util.List;
 
 public class DetalheActivity extends AppCompatActivity {
 
+    private List<String> downloadList = new ArrayList<>();
     private List<Post> postList = new ArrayList<>();
     private AdapterPost adapterPost;
 
@@ -56,10 +59,41 @@ public class DetalheActivity extends AppCompatActivity {
         configDados();
         configCliques();
         recuperaPosts();
+        recuperaDownloads();
     }
 
     private void configCliques() {
         findViewById(R.id.ibVoltar).setOnClickListener(view -> finish());
+        findViewById(R.id.btnBaixar).setOnClickListener(view -> efetuarDownloads());
+    }
+
+    private void efetuarDownloads() {
+        if (!downloadList.contains(post.getId())) {
+            downloadList.add(post.getId());
+            Download.salvar(downloadList);
+
+            Toast.makeText(this, "Download efetuado com sucesso!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Você já fez o download anteriormente!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void recuperaDownloads() {
+        DatabaseReference downloadRef = FirebaseHelper.getDatabaseReference()
+                .child("downloads");
+        downloadRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    downloadList.add(ds.getValue(String.class));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void recuperaPosts() {
