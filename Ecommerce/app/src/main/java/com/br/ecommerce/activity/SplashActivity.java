036f1a2e7c6b99c8138107053f7c765c738d9d2f
Beatriz Dadalto <biacoelho@proton.me>
2022-10-05 22:18:15 -0300

    add viewBinding

--- Ecommerce/app/src/main/java/com/br/ecommerce/activity/SplashActivity.java
+++ Ecommerce/app/src/main/java/com/br/ecommerce/activity/SplashActivity.java
@@ -2,9 +2,12 @@ package com.br.ecommerce.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
+import com.br.ecommerce.MainActivity;
 import com.br.ecommerce.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -13,32 +16,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-    }
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            hideSystemUI();
-        }
-    }
-
-
-    private void hideSystemUI() {
-        // Enables regular immersive mode.
-        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE
-                        // Set the content to appear under the system bars so that the
-                        // content doesn't resize when the system bars hide and show.
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        // Hide the nav bar and status bar
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        new Handler(getMainLooper()).postDelayed(() -> {
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        }, 2000);
     }
 
 }
