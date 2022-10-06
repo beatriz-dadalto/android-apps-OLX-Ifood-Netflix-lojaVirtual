c1fd186519d73f7737626151ad2062be5a3df5f9
Beatriz Dadalto <biacoelho@proton.me>
2022-10-05 23:10:08 -0300

    Criando layout activity de login

--- Ecommerce/app/src/main/java/com/br/ecommerce/MainActivity.java
+++ Ecommerce/app/src/main/java/com/br/ecommerce/MainActivity.java
@@ -2,13 +2,23 @@ package com.br.ecommerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.br.ecommerce.autenticacao.LoginActivity;
+import com.br.ecommerce.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnLogin.setOnClickListener(view ->
+                startActivity(new Intent(this, LoginActivity.class)));
     }
 }
---
+++ Ecommerce/app/src/main/java/com/br/ecommerce/autenticacao/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.br.ecommerce.autenticacao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.br.ecommerce.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
