cb44be8398f4535fef60e248e7ef170427e4dacd
Beatriz Dadalto <biacoelho@proton.me>
2022-10-11 16:45:00 -0300

    Selecionando imagem para as categorias

--- Ecommerce/app/src/main/java/com/br/ecommerce/fragment/loja/LojaCategoriaFragment.java
+++ Ecommerce/app/src/main/java/com/br/ecommerce/fragment/loja/LojaCategoriaFragment.java
@@ -1,24 +1,44 @@
 package com.br.ecommerce.fragment.loja;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.Manifest;
 import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.br.ecommerce.R;
 import com.br.ecommerce.databinding.DialogFormCategoriaBinding;
 import com.br.ecommerce.databinding.FragmentLojaCategoriaBinding;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.normal.TedPermission;
+
+import java.util.List;
 
 public class LojaCategoriaFragment extends Fragment {
 
+    private DialogFormCategoriaBinding categoriaBinding;
+
     private FragmentLojaCategoriaBinding binding;
     private AlertDialog dialog;
+
+    private String caminhoImagem = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -43,14 +63,75 @@ public class LojaCategoriaFragment extends Fragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext(),
                 R.style.CustomAlertDialog);
 
-        DialogFormCategoriaBinding categoriaBinding = DialogFormCategoriaBinding.
+        categoriaBinding = DialogFormCategoriaBinding.
                 inflate(LayoutInflater.from(getContext()));
 
         categoriaBinding.btnFechar.setOnClickListener(view -> dialog.dismiss());
         categoriaBinding.btnSalvar.setOnClickListener(view -> dialog.dismiss());
+        categoriaBinding.imagemCategoria.setOnClickListener(view -> verificaPermissaoGaleria());
 
         builder.setView(categoriaBinding.getRoot());
         dialog = builder.create();
         dialog.show();
     }
+
+    private void verificaPermissaoGaleria() {
+        PermissionListener permissionlistener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                abrirGaleria();
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(getContext(), "Permissão negada\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        TedPermission.create()
+                .setPermissionListener(permissionlistener)
+                .setDeniedTitle("Permissões")
+                .setDeniedMessage("Se você não aceitar a permissão não poderá acessar a galeria do dispositivo. Deseja aceitar agora?")
+                .setDeniedCloseButtonText("Não")
+                .setGotoSettingButtonText("Sim")
+                .setPermissions(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .check();
+    }
+
+    private void abrirGaleria() {
+        // acessar a pasta de midias da galeria
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        resultLauncher.launch(intent);
+    }
+
+    private final ActivityResultLauncher<Intent> resultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), result -> {
+                if (result.getResultCode() == RESULT_OK) {
+
+                    // recuperar caminho da imagem
+                    Uri imagemSelecionada = result.getData().getData();
+                    caminhoImagem = imagemSelecionada.toString();
+
+                    Bitmap bitmap;
+
+                    try {
+
+                        if (Build.VERSION.SDK_INT < 28) {
+                            bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imagemSelecionada);
+                        } else {
+                            ImageDecoder.Source source = ImageDecoder.createSource(getActivity().getContentResolver(), imagemSelecionada);
+                            bitmap = ImageDecoder.decodeBitmap(source);
+                        }
+
+                        categoriaBinding.imagemCategoria.setImageBitmap(bitmap);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+
+
+                }
+            }
+    );
 }
