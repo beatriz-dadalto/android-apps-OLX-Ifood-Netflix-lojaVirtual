3b9fe75aad3fcbdd829afa0be29f6255570e1f57
Beatriz Dadalto <biacoelho@proton.me>
2022-10-11 15:49:43 -0300

    Criando layout da DialogFormCategorias

--- Ecommerce/app/src/main/java/com/br/ecommerce/fragment/loja/LojaCategoriaFragment.java
+++ Ecommerce/app/src/main/java/com/br/ecommerce/fragment/loja/LojaCategoriaFragment.java
@@ -1,7 +1,10 @@
 package com.br.ecommerce.fragment.loja;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,13 +12,40 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.br.ecommerce.R;
+import com.br.ecommerce.databinding.FragmentLojaCategoriaBinding;
 
 public class LojaCategoriaFragment extends Fragment {
+
+    private FragmentLojaCategoriaBinding binding;
+    private AlertDialog dialog;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_loja_categoria, container, false);
+        binding = FragmentLojaCategoriaBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        configCliques();
+    }
+
+    private void configCliques() {
+        binding.btnAddCategoria.setOnClickListener(view -> showDialog());
+    }
+
+    private void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext(),
+                R.style.CustomAlertDialog);
+
+        View view = getLayoutInflater().inflate(R.layout.dialog_form_categoria, null);
+
+        builder.setView(view);
+        dialog = builder.create();
+        dialog.show();
     }
 }
