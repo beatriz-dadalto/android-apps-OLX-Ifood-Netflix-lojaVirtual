9a4a0363620ca1bd670b501d3413a39b7d618ae1
Beatriz Dadalto <biacoelho@proton.me>
2022-10-11 23:18:52 -0300

    Criando adapter de categorias

---
+++ Ecommerce/app/src/main/java/com/br/ecommerce/adapter/CategoriaAdapter.java
@@ -0,0 +1,62 @@
+package com.br.ecommerce.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.br.ecommerce.R;
+import com.br.ecommerce.model.Categoria;
+
+import java.util.List;
+
+public class CategoriaAdapter extends RecyclerView.Adapter<CategoriaAdapter.MyViewHolder> {
+
+    private List<Categoria> categoriaList;
+    private OnClick onClick;
+
+    public CategoriaAdapter(List<Categoria> categoriaList, OnClick onClick) {
+        this.categoriaList = categoriaList;
+        this.onClick = onClick;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_categoria_horizontal, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Categoria categoria = categoriaList.get(position);
+
+        holder.itemView.setOnClickListener(view -> onClick.onClickListener(categoria));
+    }
+
+    @Override
+    public int getItemCount() {
+        return categoriaList.size();
+    }
+
+    public interface OnClick {
+        void onClickListener(Categoria categoria);
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView nomeCategoria;
+        ImageView imagemCategoria;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            nomeCategoria = itemView.findViewById(R.id.nomeCategoria);
+            imagemCategoria = itemView.findViewById(R.id.imagemCategoria);
+        }
+    }
+}
