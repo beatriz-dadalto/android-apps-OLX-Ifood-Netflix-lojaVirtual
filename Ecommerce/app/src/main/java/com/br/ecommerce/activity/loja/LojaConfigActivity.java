7d535bf23d1da76cf9ee9771ec6fbf44202c592e
Beatriz Dadalto <biacoelho@proton.me>
2022-10-18 14:28:50 -0300

    Criando layout da activity de config da loja

---
+++ Ecommerce/app/src/main/java/com/br/ecommerce/activity/loja/LojaConfigActivity.java
@@ -0,0 +1,16 @@
+package com.br.ecommerce.activity.loja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.br.ecommerce.R;
+
+public class LojaConfigActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loja_config);
+    }
+}
