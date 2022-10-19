d7cccd68ad7615c3da2ff80bc9318a11f2b0d993
Beatriz Dadalto <biacoelho@proton.me>
2022-10-19 15:43:08 -0300

    Listando produtos na home do app

--- Ecommerce/app/src/main/java/com/br/ecommerce/fragment/usuario/UsuarioHomeFragment.java
+++ Ecommerce/app/src/main/java/com/br/ecommerce/fragment/usuario/UsuarioHomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
@@ -14,9 +15,11 @@ import android.widget.Toast;
 
 import com.br.ecommerce.R;
 import com.br.ecommerce.adapter.CategoriaAdapter;
+import com.br.ecommerce.adapter.LojaProdutoAdapter;
 import com.br.ecommerce.databinding.FragmentUsuarioHomeBinding;
 import com.br.ecommerce.helper.FirebaseHelper;
 import com.br.ecommerce.model.Categoria;
+import com.br.ecommerce.model.Produto;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,12 +29,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class UsuarioHomeFragment extends Fragment implements CategoriaAdapter.OnClick {
+public class UsuarioHomeFragment extends Fragment implements CategoriaAdapter.OnClick, LojaProdutoAdapter.OnClickLister {
 
     private FragmentUsuarioHomeBinding binding;
 
     private CategoriaAdapter categoriaAdapter;
     private List<Categoria> categoriaList = new ArrayList<>();
+    private LojaProdutoAdapter lojaProdutoAdapter;
+    private List<Produto> produtoList = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,11 +50,13 @@ public class UsuarioHomeFragment extends Fragment implements CategoriaAdapter.On
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        configRv();
+        configRvCategorias();
+        configRvProdutos();
         recuperaCategorias();
+        recuperaProdutos();
     }
 
-    private void configRv() {
+    private void configRvCategorias() {
         binding.rvCategorias.setLayoutManager(new LinearLayoutManager(requireContext(),
                 LinearLayoutManager.HORIZONTAL, false));
         binding.rvCategorias.setHasFixedSize(true);
@@ -83,6 +90,49 @@ public class UsuarioHomeFragment extends Fragment implements CategoriaAdapter.On
         });
     }
 
+    private void configRvProdutos() {
+        binding.rvProdutos.setLayoutManager(new GridLayoutManager(requireContext(), 2));
+        binding.rvProdutos.setHasFixedSize(true);
+        lojaProdutoAdapter = new LojaProdutoAdapter(produtoList, requireContext(), this);
+        binding.rvProdutos.setAdapter(lojaProdutoAdapter);
+    }
+
+    private void recuperaProdutos() {
+        DatabaseReference produtoRef = FirebaseHelper.getDatabaseReference()
+                .child("produtos");
+        produtoRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    produtoList.clear();
+                    for (DataSnapshot ds : snapshot.getChildren()) {
+                        Produto produto = ds.getValue(Produto.class);
+                        produtoList.add(produto);
+                    }
+                }
+
+                listEmpty();
+
+                binding.progressBar.setVisibility(View.GONE);
+                Collections.reverse(produtoList);
+                lojaProdutoAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void listEmpty() {
+        if (produtoList.isEmpty()) {
+            binding.textInfo.setText("Nenhum produto cadastrado");
+        } else {
+            binding.textInfo.setText("");
+        }
+    }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
@@ -92,5 +142,10 @@ public class UsuarioHomeFragment extends Fragment implements CategoriaAdapter.On
     @Override
     public void onClickListener(Categoria categoria) {
         Toast.makeText(requireContext(), categoria.getNome(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(Produto produto) {
+
     }
 }
