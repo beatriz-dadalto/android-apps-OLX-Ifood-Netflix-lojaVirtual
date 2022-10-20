89675243995f129db657a92a82a007e06b6d3cf6
Beatriz Dadalto <biacoelho@proton.me>
2022-10-20 00:58:27 -0300

    Criando layout da activity de detalhes do produto

---
+++ Ecommerce/app/src/main/java/com/br/ecommerce/activity/usuario/DetalhesProdutoActivity.java
@@ -0,0 +1,16 @@
+package com.br.ecommerce.activity.usuario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.br.ecommerce.R;
+
+public class DetalhesProdutoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_produto);
+    }
+}
