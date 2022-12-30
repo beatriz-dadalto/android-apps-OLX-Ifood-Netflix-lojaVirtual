612bc2e329e518a9b8fffcac2302fc2e8996de63
Beatriz Dadalto <biacoelho@proton.me>
2022-12-30 01:36:21 -0300

    Criando layout de tela de resumo do pedido

---
+++ Ecommerce/app/src/main/java/com/br/ecommerce/activity/usuario/UsuarioResumoPedidoActivity.java
@@ -0,0 +1,16 @@
+package com.br.ecommerce.activity.usuario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.br.ecommerce.R;
+
+public class UsuarioResumoPedidoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario_resumo_pedido);
+    }
+}
