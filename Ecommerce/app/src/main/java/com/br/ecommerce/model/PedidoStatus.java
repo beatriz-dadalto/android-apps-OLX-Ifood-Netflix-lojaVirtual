c6336685b50cd2078af7bf9bb01804733dff372e
Beatriz Dadalto <biacoelho@proton.me>
2023-01-20 01:15:07 -0300

    Criando classe enum para o status do pedido

--- Ecommerce/app/src/main/java/com/br/ecommerce/model/PedidoStatus.java
+++ Ecommerce/app/src/main/java/com/br/ecommerce/model/PedidoStatus.java
@@ -1,9 +1,24 @@
 package com.br.ecommerce.model;
 
 public enum PedidoStatus {
-
     PENDENTE, // 1
     APROVADO, // 2
-    CANCELADO // 3
+    CANCELADO; // 3
+
+    public static String getStatus(PedidoStatus status) {
+        String statusPedido;
+        switch (status) {
+            case PENDENTE:
+                statusPedido = "Pedente";
+                break;
+            case APROVADO:
+                statusPedido = "Aprovado";
+                break;
+            default:
+                statusPedido = "Cancelado";
+                break;
+        }
+        return statusPedido;
+    }
 
 }
