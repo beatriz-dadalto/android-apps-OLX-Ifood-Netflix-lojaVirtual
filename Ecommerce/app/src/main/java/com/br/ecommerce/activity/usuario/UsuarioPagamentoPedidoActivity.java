0c911c8d6d6b00751bdda5e3e06cd9ffd8f9006e
Beatriz Dadalto <biacoelho@proton.me>
2023-02-15 11:37:11 -0300

    Efetuando a pagamento do pedido pelo app. parte2

--- Ecommerce/app/src/main/java/com/br/ecommerce/activity/usuario/UsuarioPagamentoPedidoActivity.java
+++ Ecommerce/app/src/main/java/com/br/ecommerce/activity/usuario/UsuarioPagamentoPedidoActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.br.ecommerce.DAO.ItemDAO;
 import com.br.ecommerce.DAO.ItemPedidoDAO;
@@ -13,11 +14,14 @@ import com.br.ecommerce.helper.FirebaseHelper;
 import com.br.ecommerce.model.Endereco;
 import com.br.ecommerce.model.ItemPedido;
 import com.br.ecommerce.model.Loja;
+import com.br.ecommerce.model.Produto;
 import com.br.ecommerce.model.Usuario;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,6 +44,61 @@ public class UsuarioPagamentoPedidoActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         recuperaDados();
+
+        binding.progressBar.setOnClickListener(view -> configJSON());
+    }
+
+    private void configJSON() {
+        JsonObject dados = new JsonObject();
+        JsonArray itemList = new JsonArray();
+
+        JsonObject payer = new JsonObject();
+        JsonObject phone = new JsonObject();
+        JsonObject address = new JsonObject();
+        JsonObject payment_methods = new JsonObject();
+
+
+        String telefone = usuario.getTelefone()
+                .replace("(", "")
+                .replace(")", "")
+                .replace(" ", "");
+        phone.addProperty("area_code", telefone.substring(0,2));
+        phone.addProperty("number", telefone.substring(2,12));
+
+        address.addProperty("street_name", enderecoSelecionado.getLogradouro());
+        if (enderecoSelecionado.getNumero() != null) {
+            address.addProperty("street_number", enderecoSelecionado.getNumero());
+        }
+        address.addProperty("zip_code", enderecoSelecionado.getCep());
+        payment_methods.addProperty("installments", loja.getParcelas());
+
+        JsonObject item;
+        for (ItemPedido itemPedido : itemPedidoList) {
+            Produto produto = itemPedidoDAO.getProduto(itemPedido.getId());
+
+            item = new JsonObject();
+
+            item.addProperty("title", produto.getTitulo());
+            item.addProperty("currency_id", "BRL");
+            item.addProperty("picture_url", produto.getUrlsImagens().get(0).getCaminhoImagem());
+            item.addProperty("quantity", itemPedido.getQuantidade());
+            item.addProperty("unit_price", produto.getValorAtual());
+
+            itemList.add(item);
+            itemPedido.setNomeProduto(produto.getTitulo());
+
+            dados.add("items", itemList);
+
+            payer.addProperty("name", usuario.getNome());
+            payer.addProperty("email", usuario.getEmail());
+            payer.add("phone", phone);
+            payer.add("address", address);
+
+            dados.add("payer", payer);
+            dados.add("payment_methods", payment_methods);
+
+            Log.i("INFOTESTE", "configJSON" + dados);
+        }
     }
 
     private void recuperaDados() {
